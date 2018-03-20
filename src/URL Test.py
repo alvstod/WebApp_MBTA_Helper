c28c66a6d5cca4dbf12bf5bab400e67e36dc3c6e
Alvie Stoddard <alviestoddard@gmail.com>
2018-03-20 18:31:48 -0400

    Alvie, Dario, Heather

---
+++ src/URL Test.py
@@ -0,0 +1,3 @@
+Test = input('put name in here')
+Testsp = Test.replace(' ', '%20')
+print(Testsp)
--- src/mbta_helper.py
+++ src/mbta_helper.py
@@ -11,11 +11,21 @@ MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 # A little bit of scaffolding if you want to use it
 
 def get_json(url):
+
     """
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    print(f)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
+
+url_testing = "https://maps.googleapis.com/maps/api/geocode/json?address=Prudential%20Tower"
+print(get_json(url_testing))
+
+
 
 
 def get_lat_long(place_name):
@@ -25,8 +35,10 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
+    #print(json)
+    #place_testing = 'Harvard University'
+    #print(get_lat_long(place_testing))
     pass
-
 
 def get_nearest_station(latitude, longitude):
     """
@@ -35,8 +47,11 @@ def get_nearest_station(latitude, longitude):
     See http://realtime.mbta.com/Portal/Home/Documents for URL
     formatting requirements for the 'stopsbylocation' API.
     """
+    #stopsbylocation
     pass
-
+    #latitude = 0
+    #longitude = 0
+    #return latitude, longitude
 
 def find_stop_near(place_name):
     """
